b0673fd94b8adac048b40551af97007286c56e38
윤지 <121483296+yoonjian45@users.noreply.github.com>
2024-05-17 12:42:01 +0900

    Add files via upload

---
+++ 0517/js/script.js
@@ -0,0 +1,15 @@
+$(function () {
+    $('.tabmenu > li > a').click(function () {
+        $(this).parent().addClass('active')
+            .siblings().removeClass('active');
+        return false;
+    });
+});
+
+$('.notice li:first').click(function(){
+    $('#modal').addClass('active');
+});
+
+$('.btn').click(function(){
+    $('#modal').removeClass('active');
+});
