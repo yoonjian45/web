0000b053ce16eb24aae9547a7bd3b2d03d5832ad
윤지 <121483296+yoonjian45@users.noreply.github.com>
2024-06-03 19:05:55 +0900

    Add files via upload

---
+++ 0603-1/js/script.js
@@ -0,0 +1,7 @@
+$('.navi > li').mouseover(function(){
+    $(this).find('.sub').stop().slideDown(300);
+});
+
+$('.navi > li').mouseout(function(){
+    $(this).find('.sub').stop().slideUp(300);
+});
