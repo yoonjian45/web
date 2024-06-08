222e308886f3392bd348da0059efcf1145dcd02c
윤지 <121483296+yoonjian45@users.noreply.github.com>
2024-06-08 15:30:37 +0900

    Add files via upload

---
+++ 0608/js/script.js
@@ -0,0 +1,17 @@
+$('.navi > li').mouseover(function(){
+    $(this).find('.sub').stop().slideDown(300);
+});
+
+$('.navi > li').mouseout(function(){
+    $(this).find('.sub').stop().slideUp(300);
+});
+
+setInterval(function(){
+    $('.imgslide').delay(2000);
+    $('.imgslide').animate({marginTop : -300});
+    $('.imgslide').delay(2000);
+    $('.imgslide').animate({marginTop : -600});
+    $('.imgslide').delay(2000);
+    $('.imgslide').animate({marginTop : 0});
+    $('.imgslide').delay(2000);
+})
