22b2ef9126a311bf1f1c8a93e1d1f67a2b30fb67
윤지 <121483296+yoonjian45@users.noreply.github.com>
2024-06-24 10:46:28 +0900

    Add files via upload

---
+++ 0624/js/script.js
@@ -0,0 +1,26 @@
+$('.navi').mouseover(function(){
+    $(this).find('.sub').stop().slideDown(300);
+    $('.mbg').stop().slideDown(300);
+});
+
+$('.navi').mouseout(function(){
+    $(this).find('.sub').stop().slideUp(300);
+    $('.mbg').stop().slideUp(300);
+});
+
+setInterval(function(){
+    $('.imgslide').delay(2000);
+    $('.imgslide').animate({marginTop : -350});
+    $('.imgslide').delay(2000);
+    $('.imgslide').animate({marginTop : -700});
+    $('.imgslide').delay(2000);
+    $('.imgslide').animate({marginTop : -0});
+    $('.imgslide').delay(2000);
+});
+
+$(function(){
+    $('tabmenu > li > a').click(function(){
+        (this).parent().addCalss('actice').siblings().removerClass('active');
+    return false;
+    });
+});
