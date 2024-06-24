4143ace1eb9b8bdc075cf525363b7b8722a44120
윤지 <121483296+yoonjian45@users.noreply.github.com>
2024-06-24 18:02:18 +0900

    Add files via upload

--- 0624-2/js/script.js
+++ 0624-2/js/script.js
@@ -6,3 +6,18 @@ $('.navi > li').mouseout(function () {
     $(this).find('.sub').stop().slideUp(200)
 });
 
+
+$(function(){
+    $('.tabmenu > li > a').click(function(){
+        $(this).parent().addClass('active').siblings().removeClass('active');
+    return false;
+    });
+});
+
+$('.notice li:first').click(function(){
+    $('#modal').addClass('active');
+});
+
+$('.btn').click(function(){
+    $('#modal').removeClass('active');
+});
